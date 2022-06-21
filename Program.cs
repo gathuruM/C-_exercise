02ae3f042ce5251c12a456e2d0f31dae610315c2
gathuru-ds <gathuru.mercy19@students.dkut.ac.ke>
2022-06-21 09:32:01 +0300

    First Commit

---
+++ Program.cs
@@ -0,0 +1,15 @@
+﻿// See https://aka.ms/new-console-template for more information
+// Console.WriteLine("Hello, World!");
+
+int x = 100;
+int y = 10;
+int result;
+
+try{
+    result = x/y;
+    Console.WriteLine("The result is {0}", result);
+}
+catch{
+    Console.WriteLine("Error!");
+}
+
